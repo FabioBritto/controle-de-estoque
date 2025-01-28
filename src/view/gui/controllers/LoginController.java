b8d524400a00e26da89567175f145253c5579804
FabioBritto <fabio.tritono@gmail.com>
2025-01-28 18:00:36 -0300

    feat: Criadas classes ViewControllers

---
+++ src/view/gui/controllers/LoginController.java
@@ -0,0 +1,29 @@
+package view.gui.controllers;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+	@FXML
+	private TextField txtLogin;
+	
+	@FXML
+	private TextField txtSenha;
+	
+	@FXML
+	private Button btnEntrar;
+	
+	@FXML
+	private Label lblEsqueciSenha;
+	
+	@FXML
+	private Label lblCadastrar;
+	
+	public void onBtnEntrarAction() {
+		
+	}
+}
---
+++ src/view/gui/controllers/MainViewController.java
@@ -0,0 +1,41 @@
+package view.gui.controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import view.gui.util.Alerts;
+
+public class MainViewController implements Initializable {
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+	}
+	
+	public static synchronized <T> void loadLoginView(FXMLLoader loader) {
+		try {
+			Pane newPane = loader.load();
+			Scene scene = Main.getMainScene();
+			
+			
+			AnchorPane mainPane = (AnchorPane) scene.getRoot();
+			
+			
+			mainPane.getChildren().addAll(newPane.getChildren());
+			
+		}catch(IOException e) {
+			Alerts.showAlert("ERRO", null, "Erro ao carregar login", AlertType.ERROR);
+		}
+	}
+
+	
+	
+}
